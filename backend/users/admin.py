33b68ab6db0d3dcff169fb800a38ae5a10e11ce3
Andrey Dubov <andrey123829@yandex.ru>
2026-07-24 11:57:57 +0300

    Start project and custom User

---
+++ backend/users/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import User
+
+
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Модель пользователя в админке."""
+
+    list_display = ('username', 'email',)
+    list_filter = ('username', )
+    search_fields = ('username', 'email',)
+    ordering = ('username',)
