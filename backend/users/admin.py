334633c838d1a12ea113e0cdd1c84274d35a0540
Andrey Dubov <andrey123829@yandex.ru>
2026-07-24 20:26:27 +0300

    #1: feat: Create Django-project, custom user and isort/ruff

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 
 from .models import User
 
-
-admin.site.empty_value_display = 'Не задано'
+admin.site.empty_value_display = "Не задано"
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Модель пользователя в админке."""
 
-    list_display = ('username', 'email',)
-    list_filter = ('username', )
-    search_fields = ('username', 'email',)
-    ordering = ('username',)
+    list_display = (
+        "username",
+        "email",
+    )
+    list_filter = ("username",)
+    search_fields = (
+        "username",
+        "email",
+    )
+    ordering = ("username",)
