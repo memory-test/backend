bba13d351b2e4700691a854ac3d75231c18c6908
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-07-28 01:44:31 +1000

    feat: fix imports

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
-from backend.common.choices import Difficulty
-from backend.users import constants
+from common.choices import Difficulty
+from users import constants
 
 
 class User(AbstractUser):
