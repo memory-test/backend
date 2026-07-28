e40b1ec60c887a4d3079c7b8dc54d965689fb1d1
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-07-29 00:26:24 +1000

    build: add allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secrect')
 
 DEBUG = os.getenv('DEBUG', 'True') in ['True', '1']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'users.User'
 
