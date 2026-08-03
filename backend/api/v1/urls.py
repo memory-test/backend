e0ba30b5a4c0b4192a41523eef1f106a27b77056
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-08-03 23:23:23 +1000

    #3: fixes: delete unsed url pattern

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from api.v1.views.exercises import (
     ExerciseTypeViewSet,
     ExerciseViewSet,
-    PassExerciseView,
 )
 
 router = DefaultRouter()
@@ -12,10 +11,5 @@ router.register('exercises', ExerciseViewSet)
 router.register('exercises/types', ExerciseTypeViewSet)
 
 urlpatterns = [
-    path(
-        'pass_exercises/<int:exercise_id>/',
-        PassExerciseView.as_view(),
-        name='exercise-detail',
-    ),
     path('', include(router.urls)),
 ]
